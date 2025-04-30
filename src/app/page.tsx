f49d97db3488c10ea7b1341e54a53ce203d44a85
rezzabang <rezzabang@gmail.com>
2025-04-30 14:03:01 +0700

    inisiasi nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,56 @@
-import Image from "next/image";
+'use client';
+import React from 'react';
+import { Button, Input, DatePicker, Select, Col, Row } from 'antd';
+import type { DatePickerProps } from 'antd';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+  console.log(date, dateString);
+};
+
+const handleChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
+
+const Home = () => (
+  <div className="App" style={{ padding: '20px' }}>
+    <h1>Kredensial Rekam Medis</h1>
+    <div>A. Data Pribadi
+     <Row>
+      <Col span={24}>col</Col>
+     </Row> 
+      <div>
+        <label htmlFor="name">Nama Lengkap:</label>
+        <Input type="text" id="name" name="name" style={{ width: 160 }}/>
+      </div>
+      <div>
+        <label htmlFor="ttl">Tempat, Tanggal Lahir:</label>
+        <Input type="text" id="tempatlahir" name="tempatlahir" style={{ width: 160 }}/>
+        <DatePicker onChange={onChange} id="tanggalahir" name="tanggalahir"/>
+      </div>
+      <div>
+        <label htmlFor="jeniskelamin">Jenis Kelamin:</label>
+        <Select
+          id="jeniskelamin"
+          defaultValue="Pilih Jenis Kelamin"
+          style={{ width: 160 }}
+          onChange={handleChange}
+          options={[
+            { value: 'Laki-laki', label: 'Laki-laki' },
+            { value: 'Perempuan', label: 'Perempuan' },
+          ]}
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <Input type="email" id="email" name="email" style={{ width: 160 }}/>
+      </div>
+      <div>
+        <label htmlFor="phone">No. Telepon:</label>
+        <Input type="tel" id="phone" name="phone" style={{ width: 160 }}/>
+      </div>
     </div>
-  );
-}
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default Home;
